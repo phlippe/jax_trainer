aa04c09d1e9aadf2ac46183f67b42aa395c7326c
phlippe <phillip.lippe@googlemail.com>
2024-01-16 11:28:42 +0100

    Adjusting default NaN checking keys

--- jax_trainer/trainer/trainer.py
+++ jax_trainer/trainer/trainer.py
@@ -509,7 +509,9 @@ class TrainerModule:
                 train_loader, epoch_idx=epoch_idx, train_metrics=train_metrics
             )
             if self.trainer_config.get("detect_nans", False):
-                nan_keys = self.trainer_config.get("nan_keys", ("loss", "loss_step", "loss_epoch"))
+                nan_keys = self.trainer_config.get(
+                    "nan_keys", ("train/loss", "train/loss_step", "train/loss_epoch")
+                )
                 if isinstance(nan_keys, str):
                     nan_keys = (nan_keys,)
                 if any([np.isnan(epoch_metrics.get(key, 0.0)) for key in nan_keys]):
